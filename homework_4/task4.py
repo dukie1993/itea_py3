147dd2aadc636f0cb503daa02d46597560a394d5
Eugene <dukeunreal@gmail.com>
2020-03-08 12:18:57 +0200

    Add task 4

---
+++ homework_4/task4.py
@@ -0,0 +1,9 @@
+num_list = []
+x = int(input("Перше число: "))
+num_list.append(x)
+y = int(input("Друге число: "))
+num_list.append(y)
+z = int(input("Третє число: "))
+num_list.append(z)
+num_list.sort()
+print(num_list)

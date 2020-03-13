72d3f8c85332613155af52bee1fe0d06fcb902be
Eugene <dukeunreal@gmail.com>
2020-03-13 20:37:53 +0200

    Complete task 2

---
+++ homework_5/task2.py
@@ -0,0 +1,13 @@
+fib1 = fib2 = 1
+ 
+n = int(input("Введіть номер елементу ряду Фібоначчі: "))
+ 
+if n < 2:
+    quit()
+ 
+print(fib1, end=' ')
+print(fib2, end=' ')
+for i in range(2, n):
+    fib1, fib2 = fib2, fib1 + fib2
+    print(fib2, end=' ')
+print()

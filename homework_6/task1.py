f00553da3b9d766787f144f49ffbd7b49e793f3a
Eugene <39857660+dukie1993@users.noreply.github.com>
2020-03-15 15:31:48 +0200

    Complete task 1

--- homework_6/task1.py
+++ homework_6/task1.py
@@ -1,1 +1,21 @@
-+
+'''
+Написати програму, яка генеруватиме пароль. Пароль генерується рандомно, може 
+включати лише букви(верхнього і нижнього регістру). Користувач може вказати довжину 
+пароля. Якщо пароль користувачеві не сподобався, перегенеровувати його, доки йому сподобається. 
+Програму розбити на функції.
+'''
+
+import string
+from random import *
+def password_generator(a, b):
+    try:
+        a = int(a)
+        b = int(b)
+    except ValueError:
+        print("Incorrect input.")
+        exit()
+    characters = string.ascii_letters + string.punctuation  + string.digits
+    password =  "".join(choice(characters) for x in range(a, b))
+    print("Your password is:", password)
+
+password_generator(8, 16)

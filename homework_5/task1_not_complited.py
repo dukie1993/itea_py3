02d59d5de186dc1bda4947cb4c893ff0a370cab6
Eugene <dukeunreal@gmail.com>
2020-03-18 17:40:20 +0200

    Complete task 1

--- homework_5/task1_not_complited.py
+++
@@ -1,6 +0,0 @@
-try:
-    week_num = input("Кількість тижнів: ")
-    if week_num.isdigit():
-        print(f"Ви обрали {week_num} тижнів.")
-except ValueError as err:
-    print("Невірний формат:", err, "- введені дані повинні бути числом.")

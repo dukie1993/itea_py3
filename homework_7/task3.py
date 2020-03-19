69e728a88d47c03d1ba0a8493b268bcb0c0feee0
Eugene <dukeunreal@gmail.com>
2020-03-19 17:08:47 +0200

    Update task 3 code

--- homework_7/task3.py
+++ homework_7/task3.py
@@ -2,3 +2,19 @@
 Реалізувати шифр Віженера. Не списувати :)
 '''
 
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+ 
+text = 'message'
+key = 'word'
+ 
+encrypt_text = str()
+ 
+for i, letter in enumerate(text):
+	index = alphabet.index(letter)
+ 
+	key_index = alphabet.index(key[i % len(key)])
+ 
+	index = (index + key_index) % len(alphabet)
+	encrypt_text += alphabet[index]
+ 
+print(encrypt_text)

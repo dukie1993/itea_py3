615dedd49cf0c96008d9e726f26274dd61290b89
Eugene <dukeunreal@gmail.com>
2020-03-20 12:56:54 +0200

    Update encrypt

--- homework_7/task3.py
+++ homework_7/task3.py
@@ -1,20 +1,21 @@
 '''
 Реалізувати шифр Віженера. Не списувати :)
 '''
+def encrypt(a, b): # a = слово яке треба зашифрувати, b = слово ключ
+	alphabet = 'abcdefghijklmnopqrstuvwxyz'
+	
+	encrypt_text = str()
+	
+	for i, letter in enumerate(a):
+		index = alphabet.index(letter)
+	
+		key_index = alphabet.index(b[i % len(b)])
+	
+		index = (index + key_index) % len(alphabet)
+		encrypt_text += alphabet[index]
+	
+	print(encrypt_text)
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
- 
-text = 'message'
-key = 'word'
- 
-encrypt_text = str()
- 
-for i, letter in enumerate(text):
-	index = alphabet.index(letter)
- 
-	key_index = alphabet.index(key[i % len(key)])
- 
-	index = (index + key_index) % len(alphabet)
-	encrypt_text += alphabet[index]
- 
-print(encrypt_text)
+
+if __name__ == "__main__":
+	encrypt("hello", "key")

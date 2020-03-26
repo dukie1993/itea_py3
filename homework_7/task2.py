9e88a0a75f489a43c156b20388ab89bcc95092c9
Eugene <39857660+dukie1993@users.noreply.github.com>
2020-03-26 14:14:25 +0200

    Complete task 2

--- homework_7/task2.py
+++ homework_7/task2.py
@@ -7,3 +7,18 @@ ccagrtfsdc - Результат: 8
 aaa - Результат: 1
 '''
 
+some_text = input("Введіть деякий текст: ")
+# some_text = "afdgetryfhgunfhd" - for AREPL in vs code
+
+result = 0
+substring = ""
+
+for symbol in some_text:
+    if symbol in substring:
+        existing_symbol_index = substring.index(symbol) # індекс існуючого символу
+        substring = substring[existing_symbol_index + 1:] + symbol # обрізає substring на +1 від індексу існуючого символу і додає новий символ
+    else:
+        substring += symbol
+        result = max(result, len(substring)) # якщо substring > result тоді записує більше число в result
+
+print(f"Довжина найдовшого підрядка: {result} символів.")

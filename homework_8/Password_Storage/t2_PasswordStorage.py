7cb79aa6672f3d7930582f843f97ff4b8e7e0763
Eugene <dukeunreal@gmail.com>
2020-04-04 19:37:40 +0300

    Update code

--- homework_8/Password_Storage/t2_PasswordStorage.py
+++ homework_8/Password_Storage/t2_PasswordStorage.py
@@ -17,3 +17,31 @@ Password storage.
 інформацію у файлі за введеним ключем. Після цього пропонувати створювати новий пароль 
 або ж отримати існуючий.
 '''
+
+import string
+from time import sleep
+from random import *
+def password_generator(a=8):
+    try:
+        a = int(a)
+        if a == 0:
+            print("Ви ввели 0, повторіть спробу.")
+            password_generator(input("Введіть бажану кількість символів: "))
+        while a > 0:
+            characters = string.ascii_letters + string.punctuation  + string.digits
+            password =  "".join(choice(characters) for x in range(a))
+            print("Ваший пароль:", password)
+            repeat = input("Бажаєте повторити генерацію? - Y/N: ").lower()
+            if repeat == "y":
+                print("Повторна генерація...")
+                sleep(1)
+                password_generator(input("Введіть бажану кількість символів: "))
+            elif repeat == "n":
+                print("Завершення програми.")
+                exit()
+            break
+    except ValueError:
+        print("Повторіть введення, очікується цифра.")
+        password_generator(input("Введіть бажану кількість символів: "))
+
+password_generator(input("Введіть бажану кількість символів: "))
